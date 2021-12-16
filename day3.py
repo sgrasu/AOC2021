6c4e16f5430e5795e839d19d67bd21a8e9245d74
Michael <michael.stoltejr@gmail.com>
2021-12-16 15:57:40 -0600

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -9,12 +9,17 @@ def binaryToBase10(binary: str) -> int:
 base = defaultdict(lambda: 0)
 count = 0
 
+treeDictionary = dict()
+treeCount = defaultdict(lambda: 0)
+
 with open('day3.txt', 'r') as file:
     report = file.read().splitlines()
     for line in report:
         count += 1
         for i,b in enumerate(line):
             base[i] += int(b)
+            treeDictionary[line[:(i+1)]] = line
+            treeCount[line[:(i+1)]] += 1
 
     gamma = ''
     epsilon = ''
@@ -23,3 +28,26 @@ with open('day3.txt', 'r') as file:
         epsilon += str(int(base[x] < count/2))
     
     print(binaryToBase10(gamma)*binaryToBase10(epsilon))
+
+    oxygenKey = ''
+    while treeCount[oxygenKey] > 1 or oxygenKey == '':
+        oxygenKey += '0'
+        oxygenKeyFlip = oxygenKey[:-1] + '1'
+        print(f'{oxygenKey}: {treeCount[oxygenKey]} <=> {oxygenKeyFlip}: {treeCount[oxygenKeyFlip]}')
+        oxygenKey = oxygenKey if treeCount[oxygenKey] > treeCount[oxygenKeyFlip] else oxygenKeyFlip
+    
+    oxygenValue = binaryToBase10(treeDictionary[oxygenKey])
+    print(f'{treeDictionary[oxygenKey]} = {oxygenValue}')
+
+    c02Key = ''
+    while treeCount[c02Key] > 1  or c02Key == '':
+        c02Key += '0'
+        c02KeyFlip = c02Key[:-1] + '1'
+        print(f'{c02Key}: {treeCount[c02Key]} <=> {c02KeyFlip}: {treeCount[c02KeyFlip]}')
+        c02Key = c02Key if treeCount[c02Key] <= treeCount[c02KeyFlip] else c02KeyFlip
+    
+    c02Value = binaryToBase10(treeDictionary[c02Key])
+    print(f'{treeDictionary[c02Key]} = {c02Value}')
+
+    print(oxygenValue*c02Value)
+        
